c3a802016c02bcca4ac3af672c507e944b212c64
moshebeeri <moshe.beeri@gmail.com>
2015-12-06 01:28:41 +0200

    user phone location array

--- server/api/location/location.model.js
+++ server/api/location/location.model.js
@@ -7,8 +7,11 @@ var LocationSchema = new Schema({
 
   // The valid range of latitude in degrees is -90 and +90 for the southern and northern hemisphere
   // respectively. Longitude is in the range -180 and +180 specifying the east-west position.
-  lat: Number,
-  long: Number,
+  locations : [{
+      timestamp : Date,
+      lat : String,
+      lng : String
+    }],
   userId: String,
   timestamp: { type : Date, default: Date.now },
 
