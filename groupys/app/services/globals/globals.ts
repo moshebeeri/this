1b5e7a2d4241a71809a02a3abe125f57d53f915f
Rami Raz <rami@tabstorm.com>
2016-05-26 16:29:11 +0300

    store global dev and production urls

--- groupys/app/services/globals/globals.ts
+++ groupys/app/services/globals/globals.ts
@@ -2,10 +2,21 @@ import {Injectable} from 'angular2/core';
 
 @Injectable()
 export class GlobalsService {
-    constructor() {}
+    constructor() {
+	/*
+		if (window.location.host.match("localhost:9000")) {
+            return this.API = 'http://localhost:9000/api/';
+        } else {
+            return this.API = 'http://enter2-tabstorm.rhcloud.com/api/';
+        }
+		*/
+	
+	}
+	
 
-    LOGIN_URL: string = "http://localhost:3001/sessions/create";
-    SIGNUP_URL: string = "http://localhost:3001/users";
+    LOGIN_URL: string = "http://localhost:9000/api/local";
+    SIGNUP_URL: string = "http://localhost:9000/api/users";
+    ME_URL: string = "http://localhost:9000/api/users/me";
 }
 
 
