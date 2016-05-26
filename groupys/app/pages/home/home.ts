82db6437ca9f347229b6f151cd7385b0aedf9b44
Rami Raz <rami@tabstorm.com>
2016-05-26 16:24:16 +0300

    add roles to homepage - testing

--- groupys/app/pages/home/home.ts
+++ groupys/app/pages/home/home.ts
@@ -1,11 +1,25 @@
 import {Page} from 'ionic-angular';
+import {AuthService} from '../../services/auth/auth';
 
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor() {
+
+  isLoggedIn: boolean;
+  isAuthorized: boolean;
+  isAdmin: boolean;
+  
+  constructor(private auth: AuthService) {
+
+    this.isLoggedIn = auth.isLoggedIn();
+    this.isAuthorized = auth.isAuthorized('user');
+    this.isAdmin = auth.isAdmin();
+
+    console.log("isLoggedIn: " +  this.isLoggedIn);
+    console.log("isAuthorized: " +  this.isAuthorized);
+    console.log("isAdmin: " +  this.isAdmin);
 
   }
 }
