5726a27c89918a4f600cfa4f0dd9b89715c3a911
Rami Raz <rami@tabstorm.com>
2016-05-26 16:17:47 +0300

    Register Page - choose country, validate phone number length, create user on server only with phone number, on success registration get user details, local storage user details and roles  (user, admin, is_loggedin)

---
+++ groupys/app/pages/register/register.ts
@@ -0,0 +1,167 @@
+import {Page, NavController, Storage, LocalStorage} from 'ionic-angular';
+import {Http, Headers} from 'angular2/http';
+import {FORM_DIRECTIVES,FormBuilder,ControlGroup} from 'angular2/common';
+import {GlobalsService} from '../../services/globals/globals';
+import {AuthService} from '../../services/auth/auth';
+import {GlobalHeaders} from '../../services/headers/headers';
+import {CountriesPage} from '../countries/countries';
+import {HomePage} from '../home/home';
+const map = require('rxjs/add/operator/map');
+
+@Page({
+  templateUrl: 'build/pages/register/register.html',
+  directives: [FORM_DIRECTIVES],
+  providers : [GlobalsService, GlobalHeaders, AuthService]
+})
+export class RegisterPage {
+
+  // When the page loads, we want the Login segment to be selected
+  error: string;
+  local: Storage = new Storage(LocalStorage);
+  contentHeader: Headers = new Headers();
+  countryName:string;
+  callingCodes:string;
+  callingDigits:string;
+  digitsValidator:string;
+  callingDigitsInput:number;
+  validatorLength:number;
+  currentUser: string;
+
+  isLoggedIn: boolean;
+  isAuthorized: boolean;
+  isAdmin: boolean;
+  
+  static get parameters() {
+    return [[NavController], [Http], [GlobalsService], [GlobalHeaders], [AuthService]];
+  }
+
+  constructor(nav, http, globals, globalHeaders, auth) {
+    this.nav = nav;
+    this.http = http;
+	this.globals = globals;
+    this.globalHeaders = globalHeaders;
+    this.auth = auth;
+
+    this.isLoggedIn = this.auth.isLoggedIn();
+    this.isAuthorized = this.auth.isAuthorized('user');
+    this.isAdmin = this.auth.isAdmin();
+
+	this.contentHeader = this.globalHeaders.getMyGlobalHeaders();
+    console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxx" + JSON.stringify(this.contentHeader))	
+    this.error = null;
+    this.callingDigitsInput='';
+    this.validatorLength=-99;
+    this.getCountryDetails();
+    
+  }
+
+  validateNumbers(evt){
+	if ([48, 49, 50, 51, 52, 53, 54, 55, 56, 57].indexOf(evt.keyCode || evt.which) == -1){
+		evt.returnValue = false;
+		if(evt.preventDefault){evt.preventDefault();}
+	}
+  }
+  
+  getCountryDetails(){
+    this.local.get('countryNameDetails').then(countryNameDetails => {
+      this.countryName = JSON.parse(countryNameDetails);
+      console.log('countryName' + this.countryName);
+    }).catch(error => {
+      console.log(error);
+    });
+    this.local.get('callingCodesDetails').then(callingCodesDetails => {
+      this.callingCodes = JSON.parse(callingCodesDetails);
+      console.log('callingCodes' + this.callingCodes);
+    }).catch(error => {
+      console.log(error);
+    });
+    this.local.get('callingDigitsDetails').then(callingDigitsDetails => {
+      this.callingDigits = JSON.parse(callingDigitsDetails);
+      console.log('callingDigits' + this.callingDigits);
+    }).catch(error => {
+      console.log(error);
+    });
+	this.local.get('digitsValidator').then(digitsValidator => {
+      this.digitsValidator = JSON.parse(digitsValidator);
+      console.log('digitsValidator' + this.digitsValidator);
+	  document.getElementById("phoneInput").setAttribute("maxlength", this.digitsValidator);
+	  document.getElementById("phoneInput").getElementsByTagName( 'input' )[0].setAttribute("maxlength", this.digitsValidator);
+    }).catch(error => {
+      console.log(error);
+    });
+    
+  }
+  itemTapped() {
+    this.nav.push(CountriesPage);
+  }
+
+  register(credentials) {
+
+	if(credentials["callingDigits"] == null) {
+		this.error = 'Phone number can not be empty';
+		console.log(this.error);
+		return;
+	} else if(credentials["callingDigits"].length != this.digitsValidator){
+		this.error = 'Please enter a valid phone number';
+		console.log(this.error);
+		return;
+	}
+	let phone_number = this.callingCodes + credentials["callingDigits"];
+	credentials["phone_number"] = phone_number;
+	/*
+	credentials["username"] = 'rami raz';
+	credentials["email"] = 'ramiraz76@gmail.com';
+	credentials["password"] = 'password';
+	*/
+	console.log(JSON.stringify(credentials));
+	console.log(JSON.stringify(this.contentHeader));
+	
+    this.http.post(this.globals.SIGNUP_URL, JSON.stringify(credentials), { headers: this.contentHeader })
+        .map(res => res.json())
+        .subscribe(
+            data => this.authSuccess(data.token),
+            err => this.error = err,
+			() => console.log('Register request Complete')
+        );
+  }
+
+  logout() {
+    this.auth.logout();
+	this.contentHeader = new Headers({"Content-Type": "application/json"});
+    this.nav.setRoot(HomePage);
+  }
+
+  authSuccess(token) {
+	console.log("token---------------------------------------" + token);
+    this.error = null;
+	this.auth.setToken(token);
+	this.contentHeader = new Headers({"Content-Type": "application/json"});
+	this.contentHeader.append('Authorization', 'Bearer ' + token); 
+	  
+	console.log("contentHeader---------------------------------------" + JSON.stringify(this.contentHeader));
+	this.http.get(this.globals.ME_URL, { headers: this.contentHeader})
+        .map(res => res.json())
+        .subscribe(
+            data => this.setCurrentUser(data),
+            err => this.error = err,
+			() => this.nav.setRoot(HomePage)
+        );	
+  }
+  setCurrentUser(data){
+	this.auth.setCurrentUser(data);
+	this.currentUser = this.auth.getCurrentUser();
+	console.log(this.currentUser._id);
+	console.log( JSON.stringify(this.currentUser));
+    this.isLoggedIn = this.auth.isLoggedIn();
+    this.isAuthorized = this.auth.isAuthorized('user');
+    this.isAdmin = this.auth.isAdmin();
+  }
+
+  printObject(){
+	let output = '';
+	for (let property in object) {
+	  output += property + ': ' + object[property]+'; ';
+	}
+	console.log(output);
+  }
+}
