003d03900ce9fbbd8b3a6263a2d51353e6334241
Rami Raz <rami@tabstorm.com>
2016-06-10 00:05:05 +0300

    ADD GEOLOCATION PLUGIN
     - install cordova geolocation plugin
     - POC the plugin
     - TO DO: send the serve params and get country

--- groupys/app/pages/home/home.ts
+++ groupys/app/pages/home/home.ts
@@ -1,25 +1,41 @@
 import {Page} from 'ionic-angular';
+import {Geolocation} from 'ionic-native';
 import {AuthService} from '../../services/auth/auth';
+//import {GeolocationService} from '../../services/geolocation/geolocation';
 
 
 @Page({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  providers : [Geolocation]
 })
 export class HomePage {
 
   isLoggedIn: boolean;
   isAuthorized: boolean;
   isAdmin: boolean;
+  currentLatitude: number;
+  currentLongitude: number;
+  EM:any; // no type def for EM yet
   
   constructor(private auth: AuthService) {
 
     this.isLoggedIn = auth.isLoggedIn();
     this.isAuthorized = auth.isAuthorized('user');
     this.isAdmin = auth.isAdmin();
+	
+	Geolocation.getCurrentPosition().then((resp) => {
+		this.currentLatitude = resp.coords.latitude;
+		this.currentLongitude = resp.coords.longitude;
+        console.log("Latitude: ", resp.coords.latitude);
+        console.log("Longitude: ", resp.coords.longitude);
+      });
+	
+	//this.currentLatitude = geolocation.getLatitude();
+    //this.currentLongitude = geolocation.getLongitude();
 
-    console.log("isLoggedIn: " +  this.isLoggedIn);
-    console.log("isAuthorized: " +  this.isAuthorized);
-    console.log("isAdmin: " +  this.isAdmin);
+    console.log("isLoggedIn2: " +  this.isLoggedIn);
+    console.log("isAuthorized2: " +  this.isAuthorized);
+    console.log("isAdmin2: " +  this.isAdmin);
 
   }
 }
