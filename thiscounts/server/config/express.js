7a16a77837871706d0aeb38d2dcd6c14a6b03ee2
Rami Raz <rami@tabstorm.com>
2016-06-21 00:21:43 +0300

    SUPPORT LARGE PHONE BOOK FILES

--- thiscounts/server/config/express.js
+++ thiscounts/server/config/express.js
@@ -29,7 +29,13 @@ module.exports = function(app) {
   app.set('view engine', 'html');
   app.use(compression());
   app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+  //app.use(bodyParser.json());
+  app.use(bodyParser.json({limit: '50mb'}) );
+  app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit:50000
+  }));
   app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
