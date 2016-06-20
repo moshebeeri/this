c962f76c41712de792fbea989d22e875479c19b9
Rami Raz <rami@tabstorm.com>
2016-06-21 00:07:39 +0300

    CREATE END POINT - CHECK IF PHONE REGISTRED
    - every phone number can be checked if it's registered.
    
    - can't use it checking the contact list - the app crashes.

--- groupys/app/pages/home/home.ts
+++ groupys/app/pages/home/home.ts
@@ -1,34 +1,44 @@
 import {Page} from 'ionic-angular';
 import {Geolocation} from 'ionic-native';
 import {AuthService} from '../../services/auth/auth';
+import {Http, Headers} from 'angular2/http';
+import {GlobalsService} from '../../services/globals/globals';
+import {GlobalHeaders} from '../../services/headers/headers';
 //import {GeolocationService} from '../../services/geolocation/geolocation';
 
 
 @Page({
   templateUrl: 'build/pages/home/home.html',
-  providers : [Geolocation]
+  providers : [Geolocation, GlobalsService, GlobalHeaders]
 })
 export class HomePage {
 
+  contentHeader: Headers = new Headers();
+  error: string;
   isLoggedIn: boolean;
   isAuthorized: boolean;
   isAdmin: boolean;
   currentLatitude: number;
   currentLongitude: number;
   EM:any; // no type def for EM yet
-  
-  constructor(private auth: AuthService) {
+
+  constructor(private auth: AuthService, private globals:GlobalsService, private globalHeaders:GlobalHeaders, private http:Http) {
+
+    this.http = http;
+    this.globals = globals;
+    this.globalHeaders = globalHeaders;
+    this.contentHeader = this.globalHeaders.getMyGlobalHeaders();
 
     this.isLoggedIn = auth.isLoggedIn();
     this.isAuthorized = auth.isAuthorized('user');
     this.isAdmin = auth.isAdmin();
 	
-	Geolocation.getCurrentPosition().then((resp) => {
-		this.currentLatitude = resp.coords.latitude;
-		this.currentLongitude = resp.coords.longitude;
-        console.log("Latitude: ", resp.coords.latitude);
-        console.log("Longitude: ", resp.coords.longitude);
-      });
+	  Geolocation.getCurrentPosition().then((resp) => {
+      this.currentLatitude = resp.coords.latitude;
+      this.currentLongitude = resp.coords.longitude;
+          console.log("Latitude: ", resp.coords.latitude);
+          console.log("Longitude: ", resp.coords.longitude);
+    });
 	
 	//this.currentLatitude = geolocation.getLatitude();
     //this.currentLongitude = geolocation.getLongitude();
@@ -37,5 +47,15 @@ export class HomePage {
     console.log("isAuthorized2: " +  this.isAuthorized);
     console.log("isAdmin2: " +  this.isAdmin);
 
+  }
+
+  checkContact(number){
+    this.http.get(this.globals.PHONE_NUMBER_URL + number, { headers: this.contentHeader })
+      .map(res => res.json())
+      .subscribe(
+        data => console.log(data),
+        err => this.error = err,
+        () => console.log('Phone Number Found')
+      );
   }
 }
