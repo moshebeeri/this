9170d3b0b94493262e16b4e110c43c708c1293ea
Rami Raz <ramiraz76@gmail.com>
2016-08-07 16:55:47 +0300

    ADD BREEZEJS END POINT FOR CONTACTS
    ** TODO - TEST!

--- groupys/app/services/breezejs/entityManagerFactory.ts
+++ groupys/app/services/breezejs/entityManagerFactory.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
-import {Q} from '../../pages/breeze/q';
+import {Q} from './q';
 import {Observable} from 'rxjs/Observable';
 
 // Configure breeze with Q/ES6 Promise adapter
