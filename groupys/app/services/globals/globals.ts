ef56bba1caf29bcbe2d4745319a2be6d7ecdddb9
Rami Raz <ramiraz76@gmail.com>
2016-10-30 00:33:48 +0200

    MODIFY GLOBAL SERVICE
    1. use   public getPostUrlByEntity(entity) as proxy for dynamic submit i form builder

--- groupys/app/services/globals/globals.ts
+++ groupys/app/services/globals/globals.ts
@@ -3,6 +3,7 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class GlobalsService {
 
+    allURLS: Object;
     BASE_URL: string;
     LOGIN_URL: string;
     SIGNUP_URL: string;
@@ -12,10 +13,12 @@ export class GlobalsService {
     PHONE_BOOK_URL: string;
     PHONE_NUMBER_URL: string;
     FILE_TRANSFER_URL: string;
+    TEST_URL: string;
+    GROUP_URL: string;
 
     constructor() {
-	
-		if (window.location.host.match("localhost")) {
+        this.allURLS = [];
+		    if (window.location.host.match("localhost")) {
             this.BASE_URL = "http://localhost:9000/api/";
         } else {
             //this.BASE_URL = "http://low.la:9000/api/";
@@ -29,8 +32,20 @@ export class GlobalsService {
         this.PHONE_BOOK_URL = this.BASE_URL + "users/phonebook";
         this.PHONE_NUMBER_URL = this.BASE_URL + "users/phone_number/";
         this.FILE_TRANSFER_URL = this.BASE_URL + "images/";
+        this.TEST_URL = this.BASE_URL + "tests/";
+        this.GROUP_URL = this.BASE_URL + "groups/";
 
+        this.allURLS["Group"] = this.GROUP_URL;
+        this.allURLS["Test"] = this.TEST_URL;
 	}
+
+  public getPostUrlByEntity(entity){
+    if(this.allURLS[entity] != undefined && this.allURLS[entity].length > 0){
+      return this.allURLS[entity];
+    } else {
+      return this.BASE_URL;
+    }
+  }
 }
 
 
