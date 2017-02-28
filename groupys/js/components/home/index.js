467658df4c2cf9ae7bbbf0904f1c7028103fa94a
moshe <moshe.beeri@gmail.com>
2017-02-28 09:31:00 +0200

    fixing tabs example

--- groupys/js/components/home/index.js
+++ groupys/js/components/home/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, View, Text} from 'react-native';
 import {connect} from 'react-redux';
-import {Container, Header, Tabs} from 'native-base';
+import {Container, Header, Tabs, Tab} from 'native-base';
 
 import {openDrawer} from '../../actions/drawer';
 
@@ -19,49 +19,48 @@ class Home extends Component {  // eslint-disable-line
         }),
     };
 
-    render() { // eslint-disable-line class-methods-use-this
+    render_w() { // eslint-disable-line class-methods-use-this
         return (
             <Container theme={theme}>
                 <Header
-                    style={{ flexDirection: 'column',
-                            height: 110,
-                            elevation: 0,
-                            paddingTop: (Platform.OS === 'ios') ? 20 : 3,
-                            justifyContent: 'space-between',
-                          }}
+                    style={{
+                        flexDirection: 'column',
+                        height: 110,
+                        elevation: 0,
+                        paddingTop: (Platform.OS === 'ios') ? 20 : 3,
+                        justifyContent: 'space-between',
+                    }}
                 >
                     <HeaderContent />
                 </Header>
                 <View >
-                        <Text>HOME</Text>
-                        <Text>HOME</Text>
+                    <Text>HOME</Text>
+                    <Text>HOME</Text>
                 </View>
-
                 <Text>HOME</Text>
             </Container>
         );
     }
 
-    render_o() { // eslint-disable-line class-methods-use-this
+    render() { // eslint-disable-line class-methods-use-this
         return (
             <Container theme={theme}>
-                <Header
-                    style={{ flexDirection: 'column',
-            height: 110,
-            elevation: 0,
-            paddingTop: (Platform.OS === 'ios') ? 20 : 3,
-            justifyContent: 'space-between',
-          }}
+                <Header hasTabs
+                    style={{
+                        flexDirection: 'column',
+                        height: 110,
+                        elevation: 0,
+                        paddingTop: (Platform.OS === 'ios') ? 20 : 3,
+                        justifyContent: 'space-between',
+                    }}
                 >
                     <HeaderContent />
                 </Header>
 
-                <View >
-                    <Tabs >
-                        <TabHome tabLabel="Home"/>
-                        <TabOffer tabLabel="Offers"/>
-                    </Tabs>
-                </View>
+                <Tabs>
+                    <Tab heading="Home"><TabHome/></Tab>
+                    <Tab heading="Offers"><TabOffer/></Tab>
+                </Tabs>
             </Container>
         );
     }
