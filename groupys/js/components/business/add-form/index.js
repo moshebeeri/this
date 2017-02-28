357fb0ea103fc7af7c565d31acf9731a7c428721
roilandshut <roi.g.landshut@gmail.com>
2017-02-28 08:56:41 +0200

    add business

---
+++ groupys/js/components/business/add-form/index.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import {Image, Platform} from 'react-native';
+import {connect} from 'react-redux';
+import {actions} from 'react-native-navigation-redux-helpers';
+import {Container, Content, Text, InputGroup, Input, Button, Icon, View} from 'native-base';
+
+
+import login from './add-business-theme';
+import styles from './styles';
+//import AlertContainer from 'react-alert';
+const addIcon = require('../../../images/add-property-xxl.png');
+
+const {
+    replaceAt,
+} = actions;
+
+
+class AddBusiness extends Component {
+
+    static propTypes = {
+        replaceAt: React.PropTypes.func,
+        navigation: React.PropTypes.shape({
+            key: React.PropTypes.string,
+        }),
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+            error: '',
+            validationMessage: ''
+        };
+    }
+
+
+
+
+
+    replaceRoute(route) {
+        this.props.replaceAt('signup', {key: route}, this.props.navigation.key);
+    }
+
+
+
+
+    render() {
+        return (
+            <Container>
+
+                <Content theme={login} style={{backgroundColor: login.backgroundColor}}>
+
+
+                    <View theme={login} style={styles.AddContainer}>
+                        <Button style={styles.logoButton} onPress={}>
+                            <Icon name="ios-add-circle-outline" />
+                        </Button>
+                    </View>
+
+
+                </Content>
+            </Container>
+        );
+    }
+}
+
+
+function bindActions(dispatch) {
+    return {
+        replaceAt: (routeKey, route, key) => dispatch(replaceAt(routeKey, route, key)),
+    };
+}
+
+const mapStateToProps = state => ({
+    navigation: state.cardNavigation,
+});
+
+export default connect(mapStateToProps, bindActions)(<AddBusiness></AddBusiness>);
---
+++ groupys/js/components/business/add-form/styles.js
@@ -0,0 +1,55 @@
+
+
+import loginTheme from './add-business-theme';
+
+const React = require('react-native');
+
+const { StyleSheet, Platform } = React;
+
+module.exports = {
+  shadow: {
+    flex: 1,
+    width: 330,
+    height: 330,
+    backgroundColor: 'transparent',
+  },
+    AddContainer: {
+        paddingHorizontal: 20,
+        marginTop: 40,
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(255, 255, 255, 0.2)',
+        paddingBottom: 10,
+        flexDirection: 'row',
+        justifyContent: 'flex-start'
+    } ,
+  inputContainer: {
+    paddingHorizontal: 20,
+    marginTop: -120,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(255, 255, 255, 0.2)',
+    paddingBottom: 20,
+  },
+  forgot: {
+    alignSelf: 'flex-end',
+    marginBottom: (Platform.OS === 'ios') ? 10 : 0,
+    marginTop: (Platform.OS === 'ios') ? -10 : 0,
+  },
+    addButton: {
+    marginBottom: 10,
+    alignSelf: 'center',
+    backgroundColor: loginTheme.darkenButton,
+    paddingHorizontal: 40,
+  },
+  logoButton: {
+    paddingHorizontal: 50,
+    borderRadius: 4,
+    height: 40,
+    padding: 4,
+      backgroundColor: 'transparent'
+  },
+  transparentButton: {
+    padding: 0,
+    alignItems: 'flex-start',
+  },
+
+};
