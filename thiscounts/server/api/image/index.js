410403041df32ca03fa49644782b8829d5af18dc
moshe <moshe.beeri@gmail.com>
2017-03-02 17:48:59 +0200

    image delete and order

--- thiscounts/server/api/image/index.js
+++ thiscounts/server/api/image/index.js
@@ -6,14 +6,11 @@ var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-//router.get('/', controller.index);
-//router.get('/:id', controller.show);
 router.post('/:id', auth.isAuthenticated(), controller.create);
-router.post('/base64/:id', controller.base64_create);
+router.post('/base64/:id', auth.isAuthenticated(), controller.base64_create);
 router.post('/logo/:id', auth.isAuthenticated(), controller.logo);
-router.post('/base64/:id', controller.base64_logo);
-//router.put('/', auth.isAuthenticated(), controller.update);
-//router.patch('/:id', controller.update);
-//router.delete('/:id', controller.destroy);
+router.post('/base64/:id', auth.isAuthenticated(), controller.base64_logo);
+router.get('/order/:id/:date/:order', auth.isAuthenticated(), controller.order);
+router.get('/delete/:id/:date', auth.isAuthenticated(), controller.delete);
 
 module.exports = router;
