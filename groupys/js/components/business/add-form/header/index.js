3b8a1b2cf9f20c1d455a449940a0d6f841f0c386
roilandshut <roi.g.landshut@gmail.com>
2017-03-07 17:02:19 +0200

    css changes

---
+++ groupys/js/components/business/add-form/header/index.js
@@ -0,0 +1,65 @@
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { Icon, View, Button, InputGroup, Input,Text } from 'native-base';
+import {actions} from 'react-native-navigation-redux-helpers';
+import { openDrawer } from '../../../../actions/drawer';
+import navigateTo from '../../../../actions/sideBarNav';
+
+import styles from './styles';
+
+const {
+    replaceAt,
+} = actions;
+
+
+class AddBusinessHeader extends Component {
+
+  static propTypes = {
+    openDrawer: React.PropTypes.func,
+    navigateTo: React.PropTypes.func,
+  };
+
+  navigateTo(route) {
+    this.props.navigateTo(route, 'home');
+  }
+
+
+    replaceRoute(route) {
+        this.props.replaceAt('add-business', {key: route}, this.props.navigation.key);
+    }
+
+
+    render() {
+    return (
+
+      <View style={styles.header} >
+          <View style={{ flexDirection: 'row', alignItems: 'stretch'  }}>
+
+
+            <Button style={{ backgroundColor: 'transparent'}} iconLeft light onPress={() =>  this.replaceRoute('business') }>
+              <Icon name="ios-arrow-back" />
+              <Text> Cancel </Text>
+            </Button>
+
+          </View>
+
+      </View>
+    );
+  }
+}
+
+function bindAction(dispatch) {
+  return {
+      replaceAt: (routeKey, route, key) => dispatch(replaceAt(routeKey, route, key)),
+    openDrawer: () => dispatch(openDrawer()),
+    navigateTo: (route, homeRoute) => dispatch(navigateTo(route, homeRoute)),
+  };
+}
+
+const mapStateToProps = state => ({
+  navigation: state.cardNavigation,
+});
+
+export default connect(mapStateToProps, bindAction)(AddBusinessHeader);
