d65975863dffc851b1575c2596d0b527bc4245ab
moshe <moshe.beeri@gmail.com>
2017-04-02 17:48:26 +0300

    query promotions

--- thiscounts/server/api/promotion/index.js
+++ thiscounts/server/api/promotion/index.js
@@ -14,7 +14,8 @@ router.get('/list/by/business/:business_id', auth.isAuthenticated(), controller.
 router.get('/list/:business_id/:campaign_id', auth.isAuthenticated(), controller.campaign_promotions);
 router.post('/campaign', auth.isAuthenticated(), controller.create_campaign);
 
-router.get('/', auth.hasRole('admin'), controller.index);
+//TODO: undo remark required Admin role
+router.get('/',/* auth.hasRole('admin'),*/ controller.index);
 router.get('/:id', auth.isAuthenticated(), controller.show);
 router.post('/', auth.isAuthenticated(), controller.create);
 router.put('/:id', auth.isAuthenticated(), controller.update);
