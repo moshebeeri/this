e69acf69ef41022a7b3fad7ed3bf5f14c2eb3da4
moshe <moshe@low.la>
2017-05-09 11:54:10 +0300

    sms verification check on authorization

--- thiscounts/server/auth/auth.service.js
+++ thiscounts/server/auth/auth.service.js
@@ -38,7 +38,8 @@ function isAuthenticated() {
     .use(function(req, res, next) {
       User.findById(req.user._id, function (err, user) {
         //if (err) return next(err);
-        if (!user) return res.status(401).send('Unauthorized');
+        if (!user) {return res.status(401).send('Unauthorized')}
+        console.log("-----** " + req.url);
         //if (user.sms_code!=='') return res.status(401).send('no sms verification');
 
         req.user = user;
