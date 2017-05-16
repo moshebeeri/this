0d63104b56fed5db840f14c8838a1528e5a913b1
moshe <moshe@low.la>
2017-05-16 18:36:56 +0300

    distributor added

---
+++ thiscounts/server/components/distributor/index.js
@@ -0,0 +1,61 @@
+const gaussian = require('gaussian');
+const ss = require('simple-statistics');
+
+'use strict';
+
+function Distributor() {
+}
+
+function isSingle(promotion) {
+
+}
+
+
+Distributor.distributePromotion =
+  Distributor.prototype.distributePromotion = function (promotion) {
+    if(isSingle(promotion))
+      return [promotion];
+    switch(promotion.typ){
+
+    }
+  };
+
+
+Distributor.distributePromotions =
+  Distributor.prototype.distributePromotions = function (min, max, delta, quantity) {
+    let values = [min, max];
+    let mean = ss.mean(values);
+    let variance = ss.variance(values);
+    const distribution = gaussian(mean, variance);
+
+    let spreads = [];
+    let some_area = 0;
+    for (let value = min; value <= max; value += delta) {
+      let spread = {};
+      let from = value - delta / 2;
+      let to = value + delta / 2;
+      spread.area = distribution.cdf(to) - distribution.cdf(from);
+      spread.value = value;
+      some_area += spread.area;
+      spreads.push(spread)
+    }
+    let sumq = 0;
+
+    //http://stackoverflow.com/questions/792460/how-to-round-floats-to-integers-while-preserving-their-sum
+    //https://jsfiddle.net/cd8xqy6e/
+    let fpTotal = 0;
+    let intTotal = 0;
+    spreads.forEach((spread) => {
+      let float = (spread.area / some_area) * quantity;
+      spread.quantity = Math.round(float + fpTotal) - intTotal;
+      fpTotal += float;
+      intTotal += spread.quantity;
+      spread.area = undefined;
+      sumq += spread.quantity;
+    });
+    console.log(sumq);
+    return spreads;
+  };
+
+module.exports = Distributor;
+
