926d60b64ea4244efb033a6ed57c7d6fd403e443
moshe <moshe@low.la>
2017-05-25 16:17:56 +0300

    campaign return populated promotion

--- thiscounts/server/api/promotion/promotion.controller.js
+++ thiscounts/server/api/promotion/promotion.controller.js
@@ -197,12 +197,12 @@ exports.create_campaign = function (req, res) {
 
   create_promotion(promotion, function(err, promotion) {
     if (err) return handleError(res, err);
-    campaign.promotions = [promotion._id];
+    campaign.promotions = [promotion];
     campaign.creator = req.user._id;
     campaign.name = promotion.name;
     campaign_controller.create_campaign(campaign, function (err, campaign) {
       campaign.promotions.forEach((promotion) => {
-        promotionGraphModel.relate_ids(campaign._id, 'CAMPAIGN_PROMOTION', promotion);
+        promotionGraphModel.relate_ids(campaign._id, 'CAMPAIGN_PROMOTION', promotion._id);
       });
       if (err) return handleError(res, err);
       return res.status(201).json(campaign)
