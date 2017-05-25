fee1554470e7b8c4b7b678cbb8ca083f57dca2d9
moshe <moshe@low.la>
2017-05-25 15:05:06 +0300

    activity audience check bug

--- thiscounts/server/components/activity/activity.js
+++ thiscounts/server/components/activity/activity.js
@@ -142,7 +142,7 @@ function activity_impl(act, callback) {
       return;
     }
 
-    if(activity.audience && !_.includes(activity.audience, 'FOLLOW')) {
+    if(act.audience && !_.includes(act.audience, 'FOLLOW')) {
       update_feeds([], activity);
       return callback(null, activity)
     }
