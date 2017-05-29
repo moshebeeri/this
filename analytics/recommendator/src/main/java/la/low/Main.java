d8a724fcd4810a673f63f22be03c2ab673c28483
moshe <moshe.beeri@gmail.com>
2017-05-29 19:13:02 +0300

    recommendator

---
+++ analytics/recommendator/src/main/java/la/low/Main.java
@@ -0,0 +1,10 @@
+package la.low;
+
+/***
+ * Created by moshe on 5/29/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Recommendator");
+    }
+}
