1be68872e82e374a960cb24967a9184b21232ce7
roilandshut <roi.g.landshut@gmail.com>
2017-07-03 08:43:01 +0300

    add product category

--- groupys/js/components/product/add-form/index.js
+++ groupys/js/components/product/add-form/index.js
@@ -81,6 +81,7 @@ class AddProduct extends Component {
             business: this.props.navigation.state.params.business._id,
             info : this.state.info,
             retail_price: this.state.retail_price,
+            category: this.state.query,
 
 
 
--- thiscounts/server/api/product/product.model.js
+++ thiscounts/server/api/product/product.model.js
@@ -12,6 +12,7 @@ let ProductSchema = new Schema({
   brand: {type: Schema.ObjectId, ref: 'Brand', required: false},
   business: {type: Schema.ObjectId, ref: 'Business', required: false},
   retail_price: Number,
+  category: {type: String, required: true},
   pictures: [],
   active: Boolean
 });
