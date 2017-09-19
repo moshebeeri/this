bd6aa8f34e73cba84bebfe16b36df650dccd11bb
moshe <moshe.beeri@gmail.com>
2017-09-19 11:39:39 +0300

    assembling algorithm

--- groupys/js/actions/test/TestAssembleDisassamble.js
+++ groupys/js/actions/test/TestAssembleDisassamble.js
@@ -1,7 +1,7 @@
 /**
  * Created by roilandshut on 18/09/2017.
  */
-const assemblers =  require('./collectionAssembler');
+//const assemblers =  require('./collectionAssembler');
 const input ='{"_id":"59abe9b736ddb9754fdda096","entity":"59a4132bf7956ee14eca6d44","activity":{"_id":"59abe9b736ddb9754fdda093","instance":{"social_state":{"usages":0,"shares":0,"likes":0,"use":false,"follow":true,"saved":true,"realized":false,"share":false,"like":false},"_id":"59abe9b736ddb9754fdda08f","promotion":{"_id":"59abe9b636ddb9754fdda08d","type":"PERCENT","description":"just a test","name":"testme","creator":"59a412c7f7956ee14eca6d41","__v":1,"gid":2140,"more_than":{"values":[]},"happy_hour":{"values":[]},"early_booking":{"values":[]},"cash_back":{"values":[]},"punch_card":{"values":[]},"reduced_amount":{"values":[]},"prepay_discount":{"values":[]},"grow":{"values":[]},"doubling":{"values":[]},"increasing":{"values":[]},"x_for_y":{"values":[]},"x_plus_n_percent_off":{"values":[]},"x_plus_y":{"values":[]},"gift":{"values":[]},"percent":{"variation":"SINGLE","quantity":100,"values":[15]},"end":"2017-10-03T11:37:18.271Z","start":null,"location":{"type":"Point","lng":34.7848431,"lat":32.0910527,"coordinates":[34.7848431,32.0910527]},"system_report":false,"report":false,"created":"2017-09-03T11:38:30.917Z","entity":{"business":{"_id":"59a418a7ab96da66554f0d9e","name":"break even","address":"pinkas 13","email":"vreak@gmail.com","website":"www.breakeven.co.il","country":"israel","city":"tel aviv","state":"","type":"SMALL_BUSINESS","tax_id":"8876678","category":"246853","subcategory":"247013","creator":"59a412c7f7956ee14eca6d41","__v":1,"qrcode":"59a418a7ab96da66554f0d9f","gid":310,"pictures":[{"order":0,"date":1503926442201,"pictures":["https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-orig.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-medium.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-small.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-thumb.jpeg"]}],"location":{"type":"Point","lng":34.7848431,"lat":32.0910527,"coordinates":[34.7848431,32.0910527]},"groups":[],"created":"2017-08-28T13:20:39.959Z"}},"condition":{"business":{"_id":"59a418a7ab96da66554f0d9e","name":"break even","address":"pinkas 13","email":"vreak@gmail.com","website":"www.breakeven.co.il","country":"israel","city":"tel aviv","state":"","type":"SMALL_BUSINESS","tax_id":"8876678","category":"246853","subcategory":"247013","creator":"59a412c7f7956ee14eca6d41","__v":1,"qrcode":"59a418a7ab96da66554f0d9f","gid":310,"pictures":[{"order":0,"date":1503926442201,"pictures":["https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-orig.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-medium.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-small.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-thumb.jpeg"]}],"location":{"type":"Point","lng":34.7848431,"lat":32.0910527,"coordinates":[34.7848431,32.0910527]},"groups":[],"created":"2017-08-28T13:20:39.959Z"},"category":[]},"distribution":{"business":{"_id":"59a418a7ab96da66554f0d9e","name":"break even","address":"pinkas 13","email":"vreak@gmail.com","website":"www.breakeven.co.il","country":"israel","city":"tel aviv","state":"","type":"SMALL_BUSINESS","tax_id":"8876678","category":"246853","subcategory":"247013","creator":"59a412c7f7956ee14eca6d41","__v":1,"qrcode":"59a418a7ab96da66554f0d9f","gid":310,"pictures":[{"order":0,"date":1503926442201,"pictures":["https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-orig.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-medium.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-small.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-thumb.jpeg"]}],"location":{"type":"Point","lng":34.7848431,"lat":32.0910527,"coordinates":[34.7848431,32.0910527]},"groups":[],"created":"2017-08-28T13:20:39.959Z"},"groups":[],"cards":[]},"on_action":{"proximity":5,"active":false},"pictures":[{"order":0,"date":1504438713270,"pictures":["https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-orig.jpeg","https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-medium.jpeg","https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-small.jpeg","https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-thumb.jpeg"]}],"automatic":{"products":[],"types":[]},"validate_barcode":false},"type":"PERCENT","variation":"SINGLE","quantity":100,"remaining":100,"__v":0,"gid":2142,"value":{"percent":15,"happy_hour":{"days":[]}},"location":{"lat":32.0910527,"lng":34.7848431,"type":"Point","coordinates":[34.7848431,32.0910527]},"realizations":[]},"promotion":{"social_state":{"usages":0,"saved":false,"use":false,"realized":false,"shares":0,"likes":0,"like":false,"follow":false,"share":false},"_id":"59abe9b636ddb9754fdda08d","type":"PERCENT","description":"just a test","name":"testme","creator":"59a412c7f7956ee14eca6d41","__v":1,"gid":2140,"more_than":{"values":[]},"happy_hour":{"values":[]},"early_booking":{"values":[]},"cash_back":{"values":[]},"punch_card":{"values":[]},"reduced_amount":{"values":[]},"prepay_discount":{"values":[]},"grow":{"values":[]},"doubling":{"values":[]},"increasing":{"values":[]},"x_for_y":{"values":[]},"x_plus_n_percent_off":{"values":[]},"x_plus_y":{"values":[]},"gift":{"values":[]},"percent":{"variation":"SINGLE","quantity":100,"values":[15]},"end":"2017-10-03T11:37:18.271Z","start":null,"location":{"type":"Point","lng":34.7848431,"lat":32.0910527,"coordinates":[34.7848431,32.0910527]},"system_report":false,"report":false,"created":"2017-09-03T11:38:30.917Z","entity":{"business":{"_id":"59a418a7ab96da66554f0d9e","name":"break even","address":"pinkas 13","email":"vreak@gmail.com","website":"www.breakeven.co.il","country":"israel","city":"tel aviv","state":"","type":"SMALL_BUSINESS","tax_id":"8876678","category":"246853","subcategory":"247013","creator":"59a412c7f7956ee14eca6d41","__v":1,"qrcode":"59a418a7ab96da66554f0d9f","gid":310,"pictures":[{"order":0,"date":1503926442201,"pictures":["https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-orig.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-medium.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-small.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-thumb.jpeg"]}],"location":{"type":"Point","lng":34.7848431,"lat":32.0910527,"coordinates":[34.7848431,32.0910527]},"groups":[],"created":"2017-08-28T13:20:39.959Z"}},"condition":{"business":{"_id":"59a418a7ab96da66554f0d9e","name":"break even","address":"pinkas 13","email":"vreak@gmail.com","website":"www.breakeven.co.il","country":"israel","city":"tel aviv","state":"","type":"SMALL_BUSINESS","tax_id":"8876678","category":"246853","subcategory":"247013","creator":"59a412c7f7956ee14eca6d41","__v":1,"qrcode":"59a418a7ab96da66554f0d9f","gid":310,"pictures":[{"order":0,"date":1503926442201,"pictures":["https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-orig.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-medium.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-small.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-thumb.jpeg"]}],"location":{"type":"Point","lng":34.7848431,"lat":32.0910527,"coordinates":[34.7848431,32.0910527]},"groups":[],"created":"2017-08-28T13:20:39.959Z"},"category":[]},"distribution":{"business":{"_id":"59a418a7ab96da66554f0d9e","name":"break even","address":"pinkas 13","email":"vreak@gmail.com","website":"www.breakeven.co.il","country":"israel","city":"tel aviv","state":"","type":"SMALL_BUSINESS","tax_id":"8876678","category":"246853","subcategory":"247013","creator":"59a412c7f7956ee14eca6d41","__v":1,"qrcode":"59a418a7ab96da66554f0d9f","gid":310,"pictures":[{"order":0,"date":1503926442201,"pictures":["https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-orig.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-medium.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-small.jpeg","https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-thumb.jpeg"]}],"location":{"type":"Point","lng":34.7848431,"lat":32.0910527,"coordinates":[34.7848431,32.0910527]},"groups":[],"created":"2017-08-28T13:20:39.959Z"},"groups":[],"cards":[]},"on_action":{"proximity":5,"active":false},"pictures":[{"order":0,"date":1504438713270,"pictures":["https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-orig.jpeg","https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-medium.jpeg","https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-small.jpeg","https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-thumb.jpeg"]}],"automatic":{"products":[],"types":[]},"validate_barcode":false},"actor_business":{"social_state":{"usages":0,"shares":0,"likes":1,"use":false,"follow":true,"saved":false,"realized":false,"share":false,"like":true},"_id":"59a418a7ab96da66554f0d9e","name":"break even","address":"pinkas 13","email":"vreak@gmail.com","website":"www.breakeven.co.il","country":"israel","city":' +
     '"tel aviv","state":"","type":"SMALL_BUSINESS","tax_id":"8876678","category":"246853","subcategory":"247013","creator":"59a412c7f7956ee14eca6d41","__v":1,"qrcode":"59a418a7ab96da66554f0d9f","gid":310,"pictures":[{"order":0,"date":1503926442201,"pictures":["https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-orig.jpeg",' +
     '"https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-medium.jpeg",' +
@@ -28,14 +28,128 @@ const promotion = '{"social_state":{"usages":0,"saved":false,"use":false,"realiz
     '"https://s3.amazonaws.com/thiscounts/images/ab/Ot/Tx-thumb.jpeg"]}],"location":{"type":"Point","lng":34.7848431,"lat":32.0910527,"coordinates":[34.7848431,32.0910527]},"groups":[],"created":"2017-08-28T13:20:39.959Z"},"groups":[],"cards":[]},"on_action":{"proximity":5,"active":false},"pictures":[{"order":0,"date":1504438713270,"pictures":["https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-orig.jpeg",' +
     '"https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-medium.jpeg","https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-small.jpeg","https://s3.amazonaws.com/thiscounts/images/Zc/qJ/eg-thumb.jpeg"]}],"automatic":{"products":[],"types":[]},"validate_barcode":false}';
 
-let inputObject = JSON.parse(input);
-
-console.log(inputObject)
-
-let disparch = (json) =>{
-    console.log('-----------------------------------------')
-    console.log(JSON.stringify(json))
-    console.log('---------------------------------------------=================--')
+let min = {
+    condition: {
+        business: {
+            _id: "59a418a7ab96da66554f0d9e",
+            name: "break even",
+        }
+    }
 };
-console.log('instance: ' + JSON.stringify(assemblers.disassembler(inputObject,disparch)))
 
+/**
+ * Created by roilandshut on 04/09/2017.
+ */
+const fieldName2CollectionName = {
+    'actor_user': 'user',
+    'user': 'user',
+    'actor_group': 'group',
+    'group': 'group',
+    'promotion': 'promotion',
+    'instance': 'instance',
+    'product': 'product',
+    'business': 'business',
+    'activity': 'activity',
+    'actor_business':'business',
+};
+
+const nameToCollections = {
+    'promotion':'promotions',
+    'actor_business':'businesses',
+    'business':'businesses',
+    'activity':'activities',
+    'instance':'instances',
+    'actor_user':'user',
+    'user':'user',
+};
+
+
+
+function collectionName(key) {return fieldName2CollectionName[key];}
+//function nameToCollection(key) {return nameToCollections[key];}
+function nameToCollection(key) {return fieldName2CollectionName[key];}
+let dataSet = {
+
+};
+
+function getCollection(collection){
+    if(!dataSet[collection])
+        dataSet[collection] = {};
+    return dataSet[collection];
+}
+
+
+function dataSetCollection(dispatch,collection,object){
+    // let actionType = 'UPSERT'+collection;
+    // if(dispatch) {
+    //     //console.log(actionType)
+    //     dispatch({
+    //         type: actionType,
+    //         item: object,
+    //     });
+    // }
+    //console.log(JSON.stringify(object, null, '\t'));
+    getCollection(collection)[object._id] = object;
+}
+
+
+function dataGetCollection(collections,collection){
+    //return collections[collection];
+    return getCollection(collection);
+}
+
+function immutableObject(obj){
+    return JSON.parse(JSON.stringify(obj))
+}
+function disassembler(input,dispatch){
+    let obj = immutableObject(input);
+    Object.keys(obj).forEach(key => {
+        let collection = collectionName(key);
+        if( obj[key] && typeof obj[key] === 'object'){
+            if(!obj[key]._id ) {
+                obj[key] = disassembler(obj[key], dispatch);
+            }else if(collection){
+                let objKey = obj[key];
+                obj[key] = obj[key]._id;
+                objKey = disassembler(objKey, dispatch);
+                dataSetCollection(dispatch,collection,objKey);
+            }
+        }
+    });
+    return obj;
+}
+function printState(){
+    console.log(JSON.stringify(dataSet, null, '\t'));
+}
+function assembler(input,collections){
+    let obj = immutableObject(input);
+    Object.keys(obj).forEach(key => {
+        let collection = nameToCollection(key);
+        if(!collection){
+            if(obj[key] && typeof obj[key] === 'object')
+                obj[key] = assembler(obj[key],collections);
+        } else {
+            obj[key] = dataGetCollection(collections, collection)[obj[key]];
+            if (!obj[key]) return;
+            console.log(key);
+            obj[key] = assembler(obj[key], collections);
+        }
+    });
+    return obj;
+}
+
+
+let inputObject = JSON.parse(input); //min;
+//console.log(JSON.stringify(inputObject, null, '\t'));
+//console.log(JSON.stringify(inputObject));
+
+let dispatch = (json) =>{
+    console.log(JSON.stringify(json, null, '\t'))
+};
+let disassembled = disassembler(inputObject, dispatch);
+//console.log(JSON.stringify(disassembled, null, '\t'));
+//assemblers.printState();
+
+console.log('assemble: ' + JSON.stringify(assembler(disassembled), null, '\t'));
+
+console.log('DONE!');
--- groupys/js/actions/test/collectionAssembler.js
+++ groupys/js/actions/test/collectionAssembler.js
@@ -1,31 +1,17 @@
 /**
  * Created by roilandshut on 04/09/2017.
  */
-
-
-
-
-
-const dataSet = {
-
-};
-
 const fieldName2CollectionName = {
     'actor_user': 'user',
     'user': 'user',
     'actor_group': 'group',
     'group': 'group',
-
     'promotion': 'promotion',
     'instance': 'instance',
     'product': 'product',
     'business': 'business',
     'activity': 'activity',
     'actor_business':'business',
-    'entity.business':'business',
-    'condition.business':'business',
-    'distribution.business':'business'
-
 };
 
 const nameToCollections = {
@@ -36,63 +22,75 @@ const nameToCollections = {
     'instance':'instances',
     'actor_user':'user',
     'user':'user',
-
-
-}
+};
 
 
 
 function collectionName(key) {return fieldName2CollectionName[key];}
-function nameToCollection(key) {return nameToCollections[key];}
+//function nameToCollection(key) {return nameToCollections[key];}
+function nameToCollection(key) {return fieldName2CollectionName[key];}
+let dataSet = {
 
+};
 
-function dataSetCollection(dispacth,collection,object){
-    let actionType = 'UPSERT'+collection;
-
-
-    let dissasemblerObject = dis(object);
-    if(dispacth) {
-        console.log(actionType)
-        dispacth({
-            type: actionType,
-            item: dissasemblerObject,
-        });
-    }
+function getCollection(collection){
+    if(!dataSet[collection])
+        dataSet[collection] = {};
+    return dataSet[collection];
 }
+
+
+function dataSetCollection(dispatch,collection,object){
+    // let actionType = 'UPSERT'+collection;
+    // if(dispatch) {
+    //     //console.log(actionType)
+    //     dispatch({
+    //         type: actionType,
+    //         item: object,
+    //     });
+    // }
+    //console.log(JSON.stringify(object, null, '\t'));
+    getCollection(collection)[object._id] = object;
+}
+
 
 function dataGetCollection(collections,collection){
-    return collections[collection];
+    //return collections[collection];
+    return getCollection(collection);
 }
- function imutableObject(obj){
-    return JSON.parse(JSON.stringify(obj))
 
+function immutableObject(obj){
+    return JSON.parse(JSON.stringify(obj))
 }
-const dis = function disassembler(input,dispatch){
-    let obj = imutableObject(input)
+module.exports.disassembler = function disassembler(input,dispatch){
+    let obj = immutableObject(input);
     Object.keys(obj).forEach(key => {
         let collection = collectionName(key);
-        if(!collection || typeof obj[key] !== 'object') return;
-        if(![obj[key]._id]) return ;
-        let objKey = obj[key];
-        obj[key] = obj[key]._id;
-        disassembler(objKey,dispatch)
-        dataSetCollection(dispatch,collection,objKey);
-
-
+        if( obj[key] && typeof obj[key] === 'object'){
+            if(!obj[key]._id ) {
+                obj[key] = disassembler(obj[key], dispatch);
+            }else if(collection){
+                let objKey = obj[key];
+                obj[key] = obj[key]._id;
+                objKey = disassembler(objKey, dispatch);
+                dataSetCollection(dispatch,collection,objKey);
+            }
+        }
     });
     return obj;
-}
-
-module.exports.disassembler =dis
-
+};
+module.exports.printState =  function(){
+    console.log(JSON.stringify(dataSet, null, '\t'));
+};
 module.exports.assembler =  function(input,collections){
-    let obj = imutableObject(input)
+    let obj = immutableObject(input);
     Object.keys(obj).forEach(key => {
         let collection = nameToCollection(key);
         if(!collection) return ;
         obj[key] = dataGetCollection(collections,collection)[obj[key]];
         if(!obj[key]) return;
-        assembler(obj[key],collections);
+        console.log(key);
+        this.assembler(obj[key],collections);
     });
     return obj;
-}
+};
