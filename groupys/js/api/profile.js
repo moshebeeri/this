5ff80e94ced6e765d941848e2cc138eab2d8533c
roilandshut <roi.g.landshut@gmail.com>
2017-09-28 14:53:19 +0300

    styeling - refactoring

--- groupys/js/api/profile.js
+++ groupys/js/api/profile.js
@@ -4,63 +4,38 @@
 /**
  * Created by roilandshut on 26/03/2017.
  */
-import store from 'react-native-simple-store';
-import Timer from './LogTimer'
-import  FeedUiConverter from './feed-ui-converter'
-let feedUiConverter = new FeedUiConverter();
+import Timer from "./LogTimer";
 let timer = new Timer();
-
 class ProfileApi {
     clean_phone_number(number) {
         // remove all non digits, and then remove 0 if it is the first digit
         return number.replace(/\D/g, '').replace(/^0/, '')
     };
 
-
-    fetch(token,from, to) {
+    fetch(token, from, to) {
         return new Promise(async(resolve, reject) => {
-
             try {
                 let fromDate = new Date();
-
-                  const response = await fetch(`${server_host}/api/profiles/instances/saved/` + from + `/` + to + `/`, {
+                const response = await fetch(`${server_host}/api/profiles/instances/saved/` + from + `/` + to + `/`, {
                     method: 'GET',
                     headers: {
                         'Accept': 'application/json, text/plain, */*',
                         'Content-Type': 'application/json;charset=utf-8',
                         'Authorization': 'Bearer ' + token
-
                     }
-
-                })
-
-
+                });
                 if (response.status == '401') {
                     reject(response);
                     return;
                 }
-
                 let responseData = await response.json();
-                timer.logTime(fromDate,new Date(),'profiles','instances/saved')
-
-                // let feeds = new Array();
-                // if(responseData.length > 0) {
-                //     feeds = responseData.map(feed => feedUiConverter.createSavedPomotion(feed)).filter(function(x){
-                //         return x != undefined;
-                //     });
-                //
-                // }
+                timer.logTime(fromDate, new Date(), 'profiles', 'instances/saved');
                 resolve(responseData);
             }
             catch (error) {
-
-                // console.log('There has been a problem with your fetch operation: ' + error.message);
                 reject(error);
             }
         });
-
-
     }
 }
-
 export default ProfileApi;
