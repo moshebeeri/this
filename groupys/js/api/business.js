8dd0d67b91d3e3588f37737a9c1a9ec8ce68fb62
roilandshut <roi.g.landshut@gmail.com>
2017-09-28 14:56:25 +0300

    styeling - refactoring

--- groupys/js/api/business.js
+++ groupys/js/api/business.js
@@ -1,237 +1,173 @@
-
-
-import Timer from './LogTimer'
-
+import Timer from "./LogTimer";
 let timer = new Timer();
-class BusinessApi
-{
-   getAll(token) {
+class BusinessApi {
+    getAll(token) {
         return new Promise(async(resolve, reject) => {
-
             try {
                 let from = new Date();
-
                 const response = await fetch(`${server_host}/api/businesses/list/mine`, {
                     method: 'GET',
                     headers: {
                         'Accept': 'application/json, text/plain, */*',
                         'Content-Type': 'application/json;charset=utf-8',
                         'Authorization': 'Bearer ' + token
-
                     }
-
-                })
+                });
                 if (response.status == '401') {
                     reject(response);
                     return;
                 }
-
                 let responseData = await response.json();
-                timer.logTime(from,new Date(),'businesses','list/mine')
+                timer.logTime(from, new Date(), 'businesses', 'list/mine');
                 resolve(responseData);
-
-
             }
             catch (error) {
-
                 console.log('There has been a problem with your fetch operation: ' + error.message);
                 reject(error);
             }
         })
-
-
     }
 
-
-
-
-    getBusinessCategories(gid,token)
-    {
-
+    getBusinessCategories(gid, token) {
         return new Promise(async(resolve, reject) => {
-
             try {
                 let from = new Date();
-
-
-                const response = await fetch(`${server_host}/api/categories/business/en/` +gid, {
+                const response = await fetch(`${server_host}/api/categories/business/en/` + gid, {
                     method: 'GET',
                     headers: {
                         'Accept': 'application/json, text/plain, */*',
                         'Content-Type': 'application/json;charset=utf-8',
                         'Authorization': 'Bearer ' + token
-
                     }
+                });
+                if (response.status == '401') {
+                    reject(response);
+                    return;
+                }
+                timer.logTime(from, new Date(), 'categories', 'business/en');
+                let responseData = await response.json();
+                resolve(responseData);
+            }
+            catch (error) {
+                console.log('There has been a problem with your fetch operation: ' + error.message);
+                reject(error);
+            }
+        })
+    }
 
+    followBusiness(businessId, token) {
+        return new Promise(async(resolve, reject) => {
+            try {
+                let from = new Date();
+                let token = await store.get('token');
+                const response = await fetch(`${server_host}/api/businesses/follow/` + businessId, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json;charset=utf-8',
+                        'Authorization': 'Bearer ' + token
+                    }
                 })
                 if (response.status == '401') {
                     reject(response);
                     return;
                 }
-                timer.logTime(from,new Date(),'categories','business/en')
-
+                timer.logTime(from, new Date(), '/businesses/search', 'business/en')
                 let responseData = await response.json();
                 resolve(responseData);
             }
             catch (error) {
-
                 console.log('There has been a problem with your fetch operation: ' + error.message);
                 reject(error);
             }
         })
-
     }
 
-    followBusiness(businessId,token){
+    checkAddress(bussiness, token) {
         return new Promise(async(resolve, reject) => {
-
-            try {
-            let from = new Date();
-
-            let token = await store.get('token');
-            const response = await fetch(`${server_host}/api/businesses/follow/` +businessId, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json;charset=utf-8',
-                    'Authorization': 'Bearer ' + token
-
-                }
-
-            })
-            if (response.status == '401') {
-                reject(response);
-                return;
-            }
-            timer.logTime(from,new Date(),'/businesses/search','business/en')
-
-            let responseData = await response.json();
-            resolve(responseData);
-        }
-        catch (error) {
-
-            console.log('There has been a problem with your fetch operation: ' + error.message);
-            reject(error);
-        }
-    })
-    }
-
-    checkAddress(bussiness,token){
-        return new Promise(async(resolve, reject) => {
-
             try {
                 let from = new Date();
-
-
                 const response = await fetch(`${server_host}/api/businesses/checkAddress/`, {
                     method: 'POST',
                     headers: {
                         'Accept': 'application/json, text/plain, */*',
                         'Content-Type': 'application/json;charset=utf-8',
                         'Authorization': 'Bearer ' + token
-
                     },
                     body: JSON.stringify(bussiness)
-
                 })
-
                 if (parseInt(response.status) >= 400) {
-                    let response = {message:'Address not found',
-                                    valid:false}
+                    let response = {
+                        message: 'Address not found',
+                        valid: false
+                    }
                     resolve(response);
                     return;
                 }
-
-
-                timer.logTime(from,new Date(),'/businesses/','checkAddress')
-
+                timer.logTime(from, new Date(), '/businesses/', 'checkAddress')
                 let responseData = await response.json();
-
                 responseData.valid = true;
                 resolve(responseData);
             }
             catch (error) {
-
                 console.log('There has been a problem with your fetch operation: ' + error.message);
                 reject(error);
             }
         })
     }
 
-
-    searchBusiness(search,token)
-    {
-
+    searchBusiness(search, token) {
         return new Promise(async(resolve, reject) => {
-
             try {
                 let from = new Date();
-
-
-                const response = await fetch(`${server_host}/api/businesses/search/0/100/` +search, {
+                const response = await fetch(`${server_host}/api/businesses/search/0/100/` + search, {
                     method: 'GET',
                     headers: {
                         'Accept': 'application/json, text/plain, */*',
                         'Content-Type': 'application/json;charset=utf-8',
                         'Authorization': 'Bearer ' + token
-
                     }
-
                 })
                 if (response.status == '401') {
                     reject(response);
                     return;
                 }
-                timer.logTime(from,new Date(),'/businesses/search','business/en')
-
+                timer.logTime(from, new Date(), '/businesses/search', 'business/en')
                 let responseData = await response.json();
                 resolve(responseData);
             }
             catch (error) {
-
                 console.log('There has been a problem with your fetch operation: ' + error.message);
                 reject(error);
             }
         })
-
     }
 
-    searchBusinessByCode(qrCode,token)
-    {
-
+    searchBusinessByCode(qrCode, token) {
         return new Promise(async(resolve, reject) => {
-
             try {
                 let from = new Date();
-
-
-                const response = await fetch(`${server_host}/api/qrcodes/find/` +qrCode, {
+                const response = await fetch(`${server_host}/api/qrcodes/find/` + qrCode, {
                     method: 'GET',
                     headers: {
                         'Accept': 'application/json, text/plain, */*',
                         'Content-Type': 'application/json;charset=utf-8',
                         'Authorization': 'Bearer ' + token
-
                     }
-
                 })
                 if (response.status == '401') {
                     reject(response);
                     return;
                 }
-                timer.logTime(from,new Date(),'/businesses/search','business/en')
-
+                timer.logTime(from, new Date(), '/businesses/search', 'business/en')
                 let responseData = await response.json();
                 resolve(responseData);
             }
             catch (error) {
-
                 console.log('There has been a problem with your fetch operation: ' + error.message);
                 reject(error);
             }
         })
-
     }
 }
-
 export default BusinessApi;
