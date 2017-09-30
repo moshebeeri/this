ba5e9854de6b2ec4e9b10b8dfd1a4772ebefed6e
moshe <moshe.beeri@gmail.com>
2017-10-01 01:01:47 +0300

    cleanups

--- thiscounts/server/components/clean_mongo.js
+++ thiscounts/server/components/clean_mongo.js
@@ -16,14 +16,23 @@ for(var i = 0, len = collectionNames.length; i < len ; i++){
     }
 }
 let a = `
-  match (u:user) optional match (u)-[r]-() delete u,r
-  match (u:product) optional match (u)-[r]-() delete u,r
-  match (u:activity) optional match (u)-[r]-() delete u,r
-  match (u:campaign) optional match (u)-[r]-() delete u,r
-  match (u:group) optional match (u)-[r]-() delete u,r
-  match (u:promotion) optional match (u)-[r]-() delete u,r
-  match (u:instance) optional match (u)-[r]-() delete u,r
-  match (u:business) optional match (u)-[r]-() delete u,r
-  match (u:location) optional match (u)-[r]-() delete u,r
-  match (u:barcode) optional match (u)-[r]-() delete u,r
+match (u:user) optional match (u)-[r]-() delete u,r
+WITH count(*) as dummy
+match (u:product) optional match (u)-[r]-() delete u,r
+WITH count(*) as dummy
+match (u:activity) optional match (u)-[r]-() delete u,r
+WITH count(*) as dummy
+match (u:campaign) optional match (u)-[r]-() delete u,r
+WITH count(*) as dummy
+match (u:group) optional match (u)-[r]-() delete u,r
+WITH count(*) as dummy
+match (u:promotion) optional match (u)-[r]-() delete u,r
+WITH count(*) as dummy
+match (u:instance) optional match (u)-[r]-() delete u,r
+WITH count(*) as dummy
+match (u:business) optional match (u)-[r]-() delete u,r
+WITH count(*) as dummy
+match (u:location) optional match (u)-[r]-() delete u,r
+WITH count(*) as dummy
+match (u:barcode) optional match (u)-[r]-() delete u,r
 `;
