78ca83456d73f7d5fb98b631fb48ef9382b5ca04
roilandshut <roi.g.landshut@gmail.com>
2017-10-25 15:46:51 +0300

    add form utils

---
+++ groupys/js/utils/fromUtils.js
@@ -0,0 +1,19 @@
+
+
+const validateEmail = (email) => {
+    let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+
+const validateWebsite = (website) => {
+    var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+    return re.test(website);
+};
+
+
+
+export default {
+    validateEmail,
+    validateWebsite
+};
