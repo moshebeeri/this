442270d9f84cb76c3fd545be19c563cac50b23c3
roilandshut <roi.g.landshut@gmail.com>
2017-11-01 21:33:21 +0200

    fix webstie validation rutine

--- groupys/js/utils/fromUtils.js
+++ groupys/js/utils/fromUtils.js
@@ -7,7 +7,10 @@ const validateEmail = (email) => {
 
 
 const validateWebsite = (website) => {
-    var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+    if(!website){
+        return true;
+    }
+    let re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
     return re.test(website);
 };
 
