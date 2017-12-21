2d74cda376c1907ace1ed161c292e131d04c09b8
moshe <moshe.beeri@gmail.com>
2017-12-21 11:05:28 +0200

    report post

--- thiscounts/server/api/activity/index.js
+++ thiscounts/server/api/activity/index.js
@@ -7,11 +7,12 @@ let auth = require('../../auth/auth.service');
 let router = express.Router();
 
 router.get('/', auth.hasRole('admin'), controller.index);
-router.delete('/:id', auth.isAuthenticated(), auth.hasRole('admin'), controller.destroy);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 router.get('/:id', auth.isAuthenticated(), controller.show);
-router.post('/', auth.isAuthenticated(), controller.create);
-router.put('/:id', auth.isAuthenticated(), controller.update);
-router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.post('/', auth.hasRole('admin'), controller.create);
+router.put('/:id', auth.hasRole('admin'), controller.update);
+router.patch('/:id', auth.hasRole('admin'), controller.update);
 router.get('/share/:activity/:user', auth.isAuthenticated(), controller.share);
+router.get('/report/:id', auth.isAuthenticated(), controller.report);
 
 module.exports = router;
