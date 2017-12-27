e8b088997dc3082cbdcf3730b773b459b43ece8c
moshe <moshe.beeri@gmail.com>
2017-12-27 17:49:29 +0200

    push notification via firebase

--- thiscounts/server/components/notification/index.js
+++ thiscounts/server/components/notification/index.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 const admin = require('firebase-admin');
 const User = require('../../api/user/user.model');
 const Notification = require('../../api/notification/notification.model');
-const serviceAccount = require("../../config/keys/this-1000-firebase-adminsdk-bfa37-bc1fe08209.json");
+const serviceAccount = require("../../config/keys/this-1000-firebase-adminsdk-reo90-e33ec01e27.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -11,26 +11,7 @@ admin.initializeApp({
 });
 
 function flatten(array) {
-// mapReduce = (a -> b, (b,a) -> b, (b,a) -> b)
-  const mapReduce = (m, r) =>
-    (acc, x) => r(acc, m(x));
-// concat :: ([a],[a]) -> [a]
-  const concat = (xs, ys) =>
-    xs.concat(ys);
-// concatMap :: (a -> [b]) -> [a] -> [b]
-  const concatMap = f => xs =>
-    xs.reduce(mapReduce(f, concat), []);
-// id :: a -> a
-  const id = x =>
-    x;
-// flatten :: [[a]] -> [a]
-  const flatten =
-    concatMap(id);
-// deepFlatten :: [[a]] -> [a]
-  const deepFlatten =
-    concatMap(x =>
-      Array.isArray(x) ? deepFlatten(x) : x);
-  deepFlatten(array)
+  return [].concat.apply([], array);
 }
 
 function firebasePNS(notification, registrationTokens) {
@@ -54,9 +35,12 @@ function firebasePNS(notification, registrationTokens) {
 //   };
 // Send a message to the devices corresponding to the provided
 // registration tokens.
+  console.log(`firebasePNS ${JSON.stringify(notification)}`);
 
   if(!notification || !notification.payload || !notification.options)
     return;
+
+  console.log(`call sendToDevice ${registrationTokens}`);
 
   admin.messaging().sendToDevice(registrationTokens, notification.payload, notification.options)
     .then(function (response) {
@@ -70,23 +54,31 @@ function firebasePNS(notification, registrationTokens) {
 }
 
 function getUserFirebaseTokens(user) {
+  console.log(`user.firebase.tokens: ${user.firebase.tokens}`);
   return [_.last(user.firebase.tokens)]
 }
 
 function pnsUserDevices(notification, audience) {
+  console.log(`pnsUserDevices
+                  notification: ${JSON.stringify(notification)}
+                  audience: ${JSON.stringify(audience)}
+               `);
+
   User.find({})
     .where('_id').in(audience)
     .exec(function (err, users) {
       if (err) {
         return console.error(err);
       }
+
       let registrationTokens = flatten(users.map(user => getUserFirebaseTokens(user)));
-      firebasePNS(registrationTokens, notification)
+      console.log(`registrationTokens: ${JSON.stringify(registrationTokens)}`);
+      firebasePNS(notification, registrationTokens)
     });
 }
 
-exports.pnsUserDevices = function (notification) {
-  pnsUserDevices(notification)
+exports.pnsUserDevices = function (notification, audience) {
+  pnsUserDevices(notification, audience)
 };
 // let note = {
 //   note: type,
@@ -94,6 +86,7 @@ exports.pnsUserDevices = function (notification) {
 //   actor_user: actor_user
 // };
 exports.notify = function (note, audience) {
+  console.log('exports.notify');
   audience.forEach(to => {
     note.to = to;
     Notification.create(note, function (err, notification) {
