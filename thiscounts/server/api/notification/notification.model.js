1d98bef9dbf19fc483e86b968411327b65e9482e
moshe <moshe.beeri@gmail.com>
2018-01-01 23:36:26 +0200

    generic notification

--- thiscounts/server/api/notification/notification.model.js
+++ thiscounts/server/api/notification/notification.model.js
@@ -3,17 +3,23 @@ let mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 const utils = require('../../components/utils').createUtils();
 const autopopulate = require('mongoose-autopopulate');
+
 let Action = [
   'NONE',
   'INFO',
   'FOLLOW',
-  'APPROVE'
+  'APPROVE',
+  'OK'
 ];
 let NotificationSchema = new Schema({
-  read: {type: Boolean, default: false},
-  action: {type: String, enum: Action, default: 'NONE'},
+  title: {type: String, required: true},
+  body: {type: String, required: true},
+  note: {type: String, default: ''},
   to: {type: Schema.ObjectId, ref: 'User', required: true, index: true},
+  action: {type: String, enum: Action, default: 'NONE'},
+  read: {type: Boolean, default: false},
   timestamp: {type: Date, required: true},
+
   promotion: {type: Schema.ObjectId, ref: 'Promotion', autopopulate: true},
   instance: {type: Schema.ObjectId, ref: 'Instance', autopopulate: true},
   product: {type: Schema.ObjectId, ref: 'Product', autopopulate: true},
@@ -22,6 +28,7 @@ let NotificationSchema = new Schema({
   business: {type: Schema.ObjectId, ref: 'Business', autopopulate: true},
   mall: {type: Schema.ObjectId, ref: 'Mall', autopopulate: true},
   chain: {type: Schema.ObjectId, ref: 'ShoppingChain', autopopulate: true},
+  card: {type: Schema.ObjectId, ref: 'Card', autopopulate: true},
   activity: {type: Schema.ObjectId, ref: 'Activity', autopopulate: true},
   comment: {type: Schema.ObjectId, ref: 'Comment', autopopulate: true},
   actor_user: {type: Schema.ObjectId, ref: 'User', autopopulate: utils.userAutopopulateOptions},
@@ -29,26 +36,8 @@ let NotificationSchema = new Schema({
   actor_mall: {type: Schema.ObjectId, ref: 'Mall', autopopulate: true},
   actor_chain: {type: Schema.ObjectId, ref: 'ShoppingChain', autopopulate: true},
   actor_group: {type: Schema.ObjectId, ref: 'Group', autopopulate: true},
-  note: {type: String, default: ''},
   qrcode: {type: Schema.ObjectId, ref: 'QRCode', autopopulate: true},
   available_actions: {type: String, enum: Action},
-  payload: {
-    notification: {
-      badge: {type: String, default: ''},
-      title: {type: String, default: ''},
-      body: {type: String, default: ''},
-      bodyLocArgs: {type: String, default: ''},
-      bodyLocKey: {type: String, default: ''},
-      clickAction: {type: String, default: ''},
-      color: {type: String, default: ''},
-      icon: {type: String, default: ''},
-      sound: {type: String, default: ''},
-      tag: {type: String, default: ''},
-      titleLocArgs: {type: String, default: ''},
-      titleLocKey: {type: String, default: ''},
-    },
-    data: {}
-  },
   options: {
     priority: {type: String, default: 'normal'},
     timeToLive: {type: Number, default: 60 * 60 * 24},
--- thiscounts/server/components/notification/index.js
+++ thiscounts/server/components/notification/index.js
@@ -7,11 +7,58 @@ const serviceAccount = require("../../config/keys/this-1000-firebase-adminsdk-re
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://this-f2f45.firebaseio.com"
+  databaseURL: "https://this-1000.firebaseio.com"
 });
+
+function unread(userId, callback){
+  Notification.count({})
+    .where('to').equals(userId)
+    .where('read').equals(false)
+    .exec(callback);
+}
 
 function flatten(array) {
   return [].concat.apply([], array);
+}
+
+function toPayloadData(notification, callback){
+  let data = {};
+  let n = {
+    color: '#4ad9ff',
+    badge: '0',
+    // 'titleLocKey': '',
+    // 'titleLocArgs': '',
+    // 'sound': '',
+    // 'icon': '',
+    // 'bodyLocKey': '',
+    // 'bodyLocArgs': '',
+  };
+  if( notification.promotion ) data = {model: 'promotion'  , _id: notification.promotion._id };
+  if( notification.instance  ) data = {model: 'instance'   , _id: notification.instance._id  };
+  if( notification.product   ) data = {model: 'product'    , _id: notification.product._id   };
+  if( notification.group     ) data = {model: 'group'      , _id: notification.group._id     };
+  if( notification.user      ) data = {model: 'user'       , _id: notification.user._id      };
+  if( notification.business  ) data = {model: 'business'   , _id: notification.business._id  };
+  if( notification.mall      ) data = {model: 'mall'       , _id: notification.mall._id      };
+  if( notification.chain     ) data = {model: 'chain'      , _id: notification.chain._id     };
+  if( notification.card      ) data = {model: 'card'       , _id: notification.card._id      };
+  if( notification.activity  ) data = {model: 'activity'   , _id: notification.activity._id  };
+  if( notification.comment   ) data = {model: 'comment'    , _id: notification.comment._id   };
+  n.title =  data.title = notification.title;
+  n.body = data.body = notification.body;
+  data.note = notification.note;
+  data.action = notification.action;
+  data.notificationId = notification._id;
+  n.tag = data._id? data._id : '';
+  unread(notification.to, function(err, badge){
+    if(err) return callback(err);
+    n.badge = badge;
+    data.badge = badge;
+    return callback(null, {
+      data,
+      notification : n
+    });
+  });
 }
 
 function firebasePNS(notification, registrationTokens) {
@@ -35,44 +82,61 @@ function firebasePNS(notification, registrationTokens) {
 //   };
 // Send a message to the devices corresponding to the provided
 // registration tokens.
-  console.log(`firebasePNS ${JSON.stringify(notification)}`);
-
-  if(!notification || !notification.payload || !notification.options)
+  if (!notification || !notification.options)
     return;
-
-  console.log(`call sendToDevice ${registrationTokens}`);
-
-  admin.messaging().sendToDevice(registrationTokens, notification.payload, notification.options)
-    .then(function (response) {
-      // See the MessagingDevicesResponse reference documentation for
-      // the contents of response.
-      console.log("Successfully sent message:", response);
-    })
-    .catch(function (error) {
-      console.log("Error sending message:", error);
-    });
+  console.log(`firebasePNS options: ${JSON.stringify(notification.options)}`);
+  console.log(`firebasePNS registrationTokens: ${JSON.stringify(registrationTokens)}`);
+  toPayloadData(notification, function(err, payload) {
+    if(err) return console.error(err);
+    /*
+        let payload = {
+          data: {
+            model: 'instance',
+            _id: '5a3668ea13c88202318fedcb',
+            notificationId: '5a3668ea13c88202318fedcb',
+            title: 'My Title',
+            body: 'My Body',
+            action: 'OK'
+          },
+          notification: {
+            title: 'My Title',
+            body: 'My Body',
+            clickAction: 'CLAIM',
+            tag: 'test', //should be data._id by default
+            color: '#4ad9ff',
+            badge: '0'
+            // 'titleLocKey': '',
+            // 'titleLocArgs': '',
+            // 'sound': '',
+            // 'icon': '',
+            // 'bodyLocKey': '',
+            // 'bodyLocArgs': '',
+          }
+        };
+      */
+    //admin.messaging().sendToDevice(registrationTokens, notification.payload, notification.options)
+    admin.messaging().sendToDevice(registrationTokens, payload, notification.options)
+      .then(function (response) {
+        console.log("Successfully sent message:", response);
+      })
+      .catch(function (error) {
+        console.log("Error sending message:", error);
+      });
+  })
 }
 
 function getUserFirebaseTokens(user) {
-  console.log(`user.firebase.tokens: ${user.firebase.tokens}`);
   return [_.last(user.firebase.tokens)]
 }
 
-function pnsUserDevices(notification, audience) {
-  console.log(`pnsUserDevices
-                  notification: ${JSON.stringify(notification)}
-                  audience: ${JSON.stringify(audience)}
-               `);
-
-  User.find({})
-    .where('_id').in(audience)
-    .exec(function (err, users) {
+function pnsUserDevices(notification) {
+  User.findById(notification.to)
+    .exec(function (err, user) {
       if (err) {
         return console.error(err);
       }
-
-      let registrationTokens = flatten(users.map(user => getUserFirebaseTokens(user)));
-      console.log(`registrationTokens: ${JSON.stringify(registrationTokens)}`);
+      //flatten(users.map(user => getUserFirebaseTokens(user)));
+      let registrationTokens = getUserFirebaseTokens(user);
       firebasePNS(notification, registrationTokens)
     });
 }
@@ -80,18 +144,13 @@ function pnsUserDevices(notification, audience) {
 exports.pnsUserDevices = function (notification, audience) {
   pnsUserDevices(notification, audience)
 };
-// let note = {
-//   note: type,
-//   group: group_id,
-//   actor_user: actor_user
-// };
+
 exports.notify = function (note, audience) {
-  console.log('exports.notify');
   audience.forEach(to => {
     note.to = to;
     Notification.create(note, function (err, notification) {
       if (err) return console.error(err);
-      pnsUserDevices(notification, audience)
+      pnsUserDevices(notification)
     });
   });
 };
