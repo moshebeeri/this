ebad4b92e74fe5c882656c4f8da13ee65817528f
moshe <moshe.beeri@gmail.com>
2018-02-22 16:01:09 +0200

    one more step toward gift by other business

--- groupys/js/components/user-profile/userPreview/index.js
+++ groupys/js/components/user-profile/userPreview/index.js
@@ -6,6 +6,8 @@ import {ThisText} from '../../../ui/index';
 
 import styles from './styles'
 import strings from "../../../i18n/i18n"
+import Video from "react-native-video";
+import BusinessListView from "../../business/business-list-view/index";
 
 export default class UserPreview extends Component {
     constructor(props) {
@@ -13,6 +15,11 @@ export default class UserPreview extends Component {
     }
 
     render() {
+        let pictureSet, thumbnailURI;
+        if(this.props.user)
+            pictureSet = this.props.user.pictures[this.props.user.pictures.length-1].pictures;
+        if(pictureSet)
+            thumbnailURI = pictureSet[3];
         let userName = this.props.user? this.props.user.name : '';
         return <View style={styles.textLayout}>
             <ThisText style={{
@@ -29,7 +36,7 @@ export default class UserPreview extends Component {
                             marginLeft: 8,
                             marginRight: 8
                         }}>{userName}</ThisText>
-                        <Thumbnail square source={{uri: this.props.user.pictures[0].pictures[3]}}/>
+                        <Thumbnail square source={{uri: thumbnailURI}}/>
                     </View> :
                     <View style={styles.textLayout}>
                         <ThisText style={{
