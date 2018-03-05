f97b026c578a21a99d71932f9cdd51ce5625839e
moshe <moshe.beeri@gmail.com>
2018-03-05 16:05:11 +0200

    create temp image

--- thiscounts/server/api/image/index.js
+++ thiscounts/server/api/image/index.js
@@ -6,13 +6,15 @@ let auth = require('../../auth/auth.service');
 
 let router = express.Router();
 
+router.get('/create/image', auth.isAuthenticated(), controller.create_image);
 router.post('/:id', auth.isAuthenticated(), controller.create);
-router.post('/base64/:id', auth.isAuthenticated(), controller.base64_create);
 router.post('/logo/:id', auth.isAuthenticated(), controller.logo);
 router.post('/letterOfIncorporation/:id', auth.isAuthenticated(), controller.letterOfIncorporation);
 router.post('/identificationCard/:id', auth.isAuthenticated(), controller.identificationCard);
 
-router.post('/logo/base64/:id', auth.isAuthenticated(), controller.base64_logo);
+// router.post('/base64/:id', auth.isAuthenticated(), controller.base64_create);
+// router.post('/logo/base64/:id', auth.isAuthenticated(), controller.base64_logo);
+
 router.get('/order/:id/:date/:order', auth.isAuthenticated(), controller.order);
 router.get('/delete/:id/:date', auth.isAuthenticated(), controller.delete);
 
