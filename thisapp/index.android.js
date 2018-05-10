24d0d4f50ff5271076e2f9c8d222d1b79100b816
roilandshut <roi.g.landshut@gmail.com>
2018-05-10 21:08:05 +0300

    fix => date time format multi language supprt

--- thisapp/index.android.js
+++ thisapp/index.android.js
@@ -7,7 +7,6 @@
 import { AppRegistry } from "react-native";
 import App from "./js/startApp";
 import codePush from "react-native-code-push";
-require('moment/min/moment-with-locales.min.js');
 
 let codePushOptions = { checkFrequency: codePush.CheckFrequency.MANUAL };
 
