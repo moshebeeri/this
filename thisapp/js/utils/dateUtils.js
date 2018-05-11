55c9c026a8d70e8204c7fd6138769cd5905c236f
roilandshut <roi.g.landshut@gmail.com>
2018-05-11 13:12:45 +0300

    fix => date translation workaround for release version

--- thisapp/js/utils/dateUtils.js
+++ thisapp/js/utils/dateUtils.js
@@ -2,67 +2,75 @@
  * Created by roilandshut on 30/08/2017.
  */
 import moment from 'moment';
-
-
-
-import localizationFr from 'moment/locale/tr'
-import localizationHe from 'moment/locale/he'
-import localizationIt from 'moment/locale/it'
-import localizationEs from 'moment/locale/es'
-import localizationDe from 'moment/locale/de'
-import localizationRu from 'moment/locale/ru'
-import localizationEn from 'moment/locale/en-ie'
 import strings from "../i18n/i18n"
+
 const I18n = require('react-native-i18n');
 
-const momentLocalization = {
-    "en-US": localizationEn,
-    en: localizationEn,
-    he: localizationHe,
-    'iw-IL':localizationHe ,
-    'he-IL':localizationHe,
-    it: localizationIt,
-    fr: localizationFr,
-    es: localizationEs,
-    de: localizationDe,
-    ru: localizationRu,
-};
-
-const localization = {
-    "en-US": 'en',
-    en: 'en',
-    he: 'he',
-    'iw-IL':'he' ,
-    'he-IL':'he',
-    it: 'it',
-    fr: 'fr',
-    es: 'es',
-    de: 'de',
-    ru: 'ru',
-};
 class DateUtils {
     messageFormater(itemDate) {
+        //moment.locale(I18n.default.locale);
         let date = new Date(itemDate);
-        moment.locale('al')
         let currentYear = new Date().getYear();
         let year = date.getYear();
         if (this.isToday(date)) {
             return moment(date).format('HH:mm');
         }
-        let locale = I18n.default.locale
         if (this.isThisWeek(date)) {
-            return strings.DateAtTime.formatUnicorn(moment(date).locale(localization[locale],momentLocalization[locale]).format('dddd'), moment(date).format('HH:mm'));
+            return strings.DateAtTime.formatUnicorn(this.localize(moment(date).format('dddd'), moment(date).format('dddd')), moment(date).format('HH:mm'));
         }
         if (currentYear === year) {
-            return strings.DateAtTime.formatUnicorn(moment(date).locale(localization[locale],momentLocalization[locale]).format('DD MMMM'), moment(date).format('HH:mm'));
+            return strings.DateAtTime.formatUnicorn(this.localize(moment(date).format('MMMM'), moment(date).format('DD MMMM')), moment(date).format('HH:mm'));
         }
-        return strings.DateAtTime.formatUnicorn(moment(date).locale(localization[locale],momentLocalization[locale]).format('DD MMMM YYYY'), moment(date).format('HH:mm'));
+        return strings.DateAtTime.formatUnicorn(this.localize(moment(date).format('MMMM'), moment(date).format('DD MMMM YYYY')), moment(date).format('HH:mm'));
+    }
+
+    localize(string, toLocale) {
+        switch (string) {
+            case 'Sunday':
+                return toLocale.replace(string, strings.Sunday)
+            case 'Monday':
+                return toLocale.replace(string, strings.Monday)
+            case 'Tuesday':
+                return toLocale.replace(string, strings.Tuesday)
+            case 'Wednesday':
+                return toLocale.replace(string, strings.Wednesday)
+            case 'Thursday':
+                return toLocale.replace(string, strings.Thursday)
+            case 'Friday':
+                return toLocale.replace(string, strings.Friday)
+            case 'Saturday':
+                return toLocale.replace(string, strings.Friday)
+            case 'January':
+                return toLocale.replace(string, strings.January)
+            case 'February':
+                return toLocale.replace(string, strings.February)
+            case 'March':
+                return toLocale.replace(string, strings.March)
+            case 'April':
+                return toLocale.replace(string, strings.April)
+            case 'May':
+                return toLocale.replace(string, strings.May)
+            case 'June':
+                return toLocale.replace(string, strings.June)
+            case 'July':
+                return toLocale.replace(string, strings.July)
+            case 'August':
+                return toLocale.replace(string, strings.August)
+            case 'September':
+                return toLocale.replace(string, strings.September)
+            case 'October':
+                return toLocale.replace(string, strings.October)
+            case 'November':
+                return toLocale.replace(string, strings.November)
+            case 'December':
+                return toLocale.replace(string, strings.December)
+        }
+        return toLocale;
     }
 
     isToday(itemDate) {
         return moment(itemDate).isSame(moment(), 'day');
     }
-
 
     isThisWeek(date) {
         var now = moment();
