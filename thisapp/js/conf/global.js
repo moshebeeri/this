a8f4f91c7f0d35c381ac9fddbffa6d95e73cbf08
roilandshut <roi.g.landshut@gmail.com>
2018-06-19 14:43:52 +0300

    code-push => fix ios

--- thisapp/js/conf/global.js
+++ thisapp/js/conf/global.js
@@ -7,6 +7,7 @@ module.exports = {
 };
 global.server_host = Config.API_URL;
 global.help_url = Config.HELP_URL;
+global.codepush = Config.CODE_PUSH_IOS;
 global.versionClient = '1.0.14';
 global.debug = true;
 global.appBackgroundColor= '#E6E6E6';
